216cb79a79d439a8e7c12bf657a9db46bebb49df
m0tbka <66780351+m0tbka@users.noreply.github.com>
2022-07-13 12:45:16 +0500

    Previous float counting system was deleted and changed to new 3 functions in the whole project

--- my_strategy.py
+++ my_strategy.py
@@ -5,11 +5,12 @@ from model.order import Order
 from model.constants import Constants
 from typing import Optional
 from debug_interface import DebugInterface
+from debugging.color import Color
 
-from strategy.geometry import Vec, Line, Segment, Circle, Float
+from strategy.float import equal_float, lt_float, le_float
+from strategy.geometry import Vec, Line, Segment, Circle
 from strategy.physics import Charge, ObstacleCharge, ZoneCharge
 from strategy.simulation import simulations_around, count_charge
-from strategy.float import Float
 
 from math import pi
 from time import time
@@ -33,13 +34,13 @@ class MyStrategy:
         MyStrategy.ObstacleCharges = {}
         for obs in constants.obstacles:
             MyStrategy.ObstacleCharges[obs.id] = ObstacleCharge(Vec(obs.position.x, obs.position.y), -10_000,
-                                                                Float(obs.radius + 1.5))
+                                                                obs.radius + 1.5)
 
     def init(self, game: Game):
         self.zone = ZoneCharge(
             Vec(game.zone.next_center.x, game.zone.next_center.y), 1_000,
-            Circle(Vec(game.zone.current_center.x, game.zone.current_center.y), Float(game.zone.current_radius - 1)),
-            Circle(Vec(game.zone.next_center.x, game.zone.next_center.y), Float(game.zone.next_radius - 1)))
+            Circle(Vec(game.zone.current_center.x, game.zone.current_center.y), game.zone.current_radius - 1),
+            Circle(Vec(game.zone.next_center.x, game.zone.next_center.y), game.zone.next_radius - 1))
         self.my_units = []
         self.near_obstacles = {}
         for unit in game.units:
@@ -48,7 +49,7 @@ class MyStrategy:
                 self.near_obstacles[unit.id] = {}
                 for id_obs, obs in MyStrategy.ObstacleCharges.items():
                     obs: Charge
-                    if (obs.centre_pos - unit.position).len <= 15:
+                    if le_float((obs.centre_pos - unit.position).len, 15):
                         self.near_obstacles[unit.id][id_obs] = obs
                 print("nearer_obs:", len(self.near_obstacles[unit.id]))
 
@@ -62,18 +63,23 @@ class MyStrategy:
         obsts = MyStrategy.ObstacleCharges
         orders = {}
 
+        # print("Visible units:")
+        # for unit in game.units:
+        #     print(unit)
+        # print("EnD")
+
         for unit in self.my_units:
-            print(f"{unit.id}: {unit.position}, {unit.velocity}, {unit.direction}")
+            print(f"Unit {unit.id}: {unit.position}, {unit.velocity}, {unit.direction}")
 
             max_simulated_pos = simulations_around(self.near_obstacles[unit.id], self.zone,
                                                    Vec(unit.position.x, unit.position.y),
-                                                   step=Float(10 / 3), angle=Float(pi / 360))
+                                                   step=10 / 3, angle=pi / 360)
 
             now_charge = count_charge(self.near_obstacles[unit.id], self.zone, Vec(unit.position.x, unit.position.y))
 
             print(count_charge(self.near_obstacles[unit.id], self.zone, max_simulated_pos), now_charge)
 
-            if count_charge(self.near_obstacles[unit.id], self.zone, max_simulated_pos) > now_charge:
+            if lt_float(now_charge, count_charge(self.near_obstacles[unit.id], self.zone, max_simulated_pos)):
                 orders[unit.id] = UnitOrder(
                     (max_simulated_pos - Vec(unit.position.x, unit.position.y)) * (consts.max_unit_forward_speed * 2),
                     (max_simulated_pos - Vec(unit.position.x, unit.position.y)) * 2, None)
