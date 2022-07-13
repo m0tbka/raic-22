80fa03cb5cecbaaf9db7431d725914956146ac7c
m0tbka <66780351+m0tbka@users.noreply.github.com>
2022-07-13 12:46:18 +0500

    Previous float counting system was deleted and changed to new 3 functions in the whole project

--- physics.py
+++ physics.py
@@ -1,19 +1,20 @@
 """Main physics: potential field, movement"""
 
-from strategy.geometry import Vec, Line, Segment, Circle, Float
+from strategy.float import equal_float, lt_float, le_float
+from strategy.geometry import Vec, Line, Segment, Circle
 
 
 class Charge:
     """Charges, their parameters"""
 
-    INF = Float(1e9)
+    INF = 1e9
     """Infinity charge"""
 
-    quantity_charge: Float
+    quantity_charge: float
     centre_pos: Vec
     field_rule: object
 
-    def __init__(self, centre_pos: Vec, quantity_charge: Float,
+    def __init__(self, centre_pos: Vec, quantity_charge: float,
                  field_rule=None):
         self.centre_pos = centre_pos
         """Central position of charge"""
@@ -28,7 +29,7 @@ class Charge:
         """Standard rule of distribution charge"""
         return self.quantity_charge
 
-    def get_charge_at_pos(self, pos: Vec, *args):
+    def get_charge_at_pos(self, pos: Vec):
         """Gives quantity of charge at 'pos' in terms of rule(field_rule)"""
         return self.field_rule(pos)
 
@@ -36,16 +37,16 @@ class Charge:
 class ObstacleCharge(Charge):
     """Charge of obstacles"""
 
-    obs_radius: Float
+    obs_radius: float
 
-    def __init__(self, centre_pos: Vec, quantity_charge: Float, obs_radius: Float):
+    def __init__(self, centre_pos: Vec, quantity_charge: float, obs_radius: float):
         super().__init__(centre_pos, quantity_charge)
         self.obs_radius = obs_radius
         """Radius of an obstacle"""
         self.const_circle = Circle(centre_pos, obs_radius)
         """Circle of constant charge"""
 
-    def rule(self, position: Vec) -> Float:
+    def rule(self, position: Vec) -> float:
         """Rule for obstacles"""
 
         if position in self.const_circle:
@@ -53,7 +54,7 @@ class ObstacleCharge(Charge):
             return self.quantity_charge
 
         """Position isn't near obstacle -> position is OK"""
-        return Float(0)
+        return 0
 
     def __repr__(self):
         return "ObstacleCharge(" + \
@@ -71,14 +72,14 @@ class ZoneCharge(Charge):
     current_zone: Circle
     next_zone: Circle
 
-    def __init__(self, centre_pos: Vec, quantity_charge: Float, current_zone: Circle, next_zone: Circle):
+    def __init__(self, centre_pos: Vec, quantity_charge: float, current_zone: Circle, next_zone: Circle):
         super().__init__(centre_pos, quantity_charge)
         self.current_zone = current_zone
         """Circle of current zone"""
         self.next_zone = next_zone
         """Circle of next zone. Center_pos equal to next_zone.center_pos"""
 
-    def rule(self, position: Vec) -> Float:
+    def rule(self, position: Vec) -> float:
         """Rule for zone"""
 
         length_pos_current = (position - self.current_zone.centre_point).len
@@ -86,37 +87,28 @@ class ZoneCharge(Charge):
         length_pos_next = (position - self.next_zone.centre_point).len
         """Distance from position to centre of next_zone"""
 
-        if length_pos_next < self.next_zone.radius:
+        if lt_float(length_pos_next, self.next_zone.radius):
             """Position in next_zone -> zone is OK"""
 
             return self.quantity_charge
 
-        if length_pos_current < self.current_zone.radius:
+        if lt_float(length_pos_current, self.current_zone.radius):
             """Position in current_zone -> need to go to the next_zone"""
 
-            segment_center_pos = Segment(position, self.centre_pos)
-            """Segment(1) from center of next_zone to position"""
-
-            intersection_point_next = self.next_zone.intersection_with_line(segment_center_pos)
-            "Intersection segment and next_zone circle"
-            if intersection_point_next[0] in segment_center_pos:
-                intersection_point_next = intersection_point_next[0]
-            else:
-                intersection_point_next = intersection_point_next[1]
+            intersection_point_next = self.next_zone.intersection_with_segment(position=position)[0]
+            "Intersection of a segment and next_zone circle"
 
             length_intersection = (position - intersection_point_next).len
-            """Length of a segment from position to intersection of next_zone circle and segment(1)"""
+            """Length of a segment from position to intersection of next_zone circle and 
+                segment from center of next_zone to position"""
 
-            intersection_point_current = self.current_zone.intersection_with_line(segment_center_pos)
-            """Intersection segment and current_zone circle"""
-            if (intersection_point_current[0] - self.centre_pos) * (position - self.centre_pos) > 0:
-                intersection_point_current = intersection_point_current[0]
-            else:
-                intersection_point_current = intersection_point_current[1]
+            intersection_point_current = self.current_zone.intersection_with_segment(
+                position=(position - self.centre_pos) * 10)[0]
+            """Intersection of a segment and current_zone circle"""
 
             length_next_current = (intersection_point_current - intersection_point_next).len
-            """Length of a segment from intersection of current_zone circle and segment(1)
-                to intersection of next_zone circle and segment(1)"""
+            """Length of a segment from intersection of current_zone circle and segment from center of next_zone to 
+                position to intersection of next_zone circle and segment from center of next_zone to position"""
 
             return self.quantity_charge - (length_intersection / length_next_current) * self.quantity_charge
 
@@ -136,6 +128,5 @@ class ZoneCharge(Charge):
 class ProjectileCharge(Charge):
     """Charge of projectiles"""
 
-    def __init__(self, centre_pos: Vec, quantity_charge: Float):
+    def __init__(self, centre_pos: Vec, quantity_charge: float):
         super().__init__(centre_pos, quantity_charge)
-
