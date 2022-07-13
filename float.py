893de0bb6575ad92a326145dfaf8e313784de612
m0tbka <66780351+m0tbka@users.noreply.github.com>
2022-07-13 12:40:55 +0500

    Absolutely rewritten that now it works well.. i believe perfectly

--- float.py
+++ float.py
@@ -1,31 +1,63 @@
 """Overloaded float functions"""
 
+after_point = 6
+epsilon = 1e-6
 
-class Float(float):
-    """Методы не вызываются!!!"""
-    EPS = 1e-9
 
-    def __eq__(self, other):
-        """self == other"""
-        return abs(self - other) < Float.EPS
+def equal_float(self, other):
+    """self == other"""
+    return abs(round(self - epsilon * 0.5, after_point) - round(other - epsilon * 0.5, after_point)) <= epsilon
 
-    def __ne__(self, other):
-        """self != other"""
-        return not self == other
 
-    def __lt__(self, other):
-        """self < other"""
-        print("FLOAT:", self, other, self - other)
-        return self - other < Float.EPS
+def lt_float(self, other):
+    """self < other"""
+    return round(self - epsilon * 0.5, after_point) - round(other - epsilon * 0.5, after_point) < epsilon
 
-    def __le__(self, other):
-        """self <= other"""
-        return self - other <= Float.EPS
 
-    def __gt__(self, other):
-        """self > other"""
-        return self - other > Float.EPS
+def le_float(self, other):
+    """self <= other"""
+    return round(self - epsilon * 0.5, after_point) - round(other - epsilon * 0.5, after_point) <= epsilon
 
-    def __ge__(self, other):
-        """self >= other"""
-        return self - other >= Float.EPS
+
+# class Float(float):
+#     EPS = 1e-9
+#
+#     def __add__(self, other):
+#         """self + other"""
+#         return Float(super(Float, self).__add__(other))
+#
+#     def __sub__(self, other):
+#         """self - other"""
+#         return Float(super(Float, self).__sub__(other))
+#
+#     def __mul__(self, other):
+#         """self * other"""
+#         return Float(super(Float, self).__mul__(other))
+#
+#     def __truediv__(self, other):
+#         """self / other"""
+#         return Float(super(Float, self).__truediv__(other))
+#
+#     def __eq__(self, other):
+#         """self == other"""
+#         return super(Float, Float(abs(self - other))).__eq__(Float.EPS)
+#
+#     def __ne__(self, other):
+#         """self != other"""
+#         return not self == other
+#
+#     def __lt__(self, other):
+#         """self < other"""
+#         return super(Float, self - other).__lt__(Float.EPS)
+#
+#     def __le__(self, other):
+#         """self <= other"""
+#         return super(Float, self - other).__le__(Float.EPS)
+#
+#     def __gt__(self, other):
+#         """self > other"""
+#         return super(Float, self - other).__gt__(Float.EPS)
+#
+#     def __ge__(self, other):
+#         """self >= other"""
+#         return super(Float, self - other).__ge__(Float.EPS)
