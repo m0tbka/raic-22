03175cd50f8531cbc0fce92624b74d4e8130fb34
m0tbka <66780351+m0tbka@users.noreply.github.com>
2022-07-13 12:39:34 +0500

    Fixed very important bug with dot/cross_product and added new function - count_angle

--- geometry.py
+++ geometry.py
@@ -2,20 +2,22 @@
 
 from model.vec2 import Vec2
 
-from strategy.float import Float
+from strategy.float import equal_float, le_float, lt_float
 
-from math import pi, sqrt, cos, sin
+from math import pi, sqrt, cos, sin, atan2
 
 
 class Vec(Vec2):
     """Vectors with useful operations"""
 
-    def __init__(self, x: Float = 0, y: Float = 0):
+    len: float
+
+    def __init__(self, x: float = 0, y: float = 0):
         super(Vec, self).__init__(x, y)
         self.len = self.get_len()
         """The length of the vector"""
 
-    def get_len(self) -> Float:
+    def get_len(self) -> float:
         """Returns the length of the vector"""
         return (self.x ** 2 + self.y ** 2) ** 0.5
 
@@ -50,7 +52,7 @@ class Vec(Vec2):
         if type(other) is Vec:
             """Counts mult of two vectors(dot_product)"""
             other: Vec
-            new = self.x * other.x + self.y + other.y
+            new = self.x * other.x + self.y * other.y
 
         else:
             """Counts mult of vector and number"""
@@ -65,35 +67,39 @@ class Vec(Vec2):
         """Counts cross_product of two vector"""
 
         other: Vec
-        return self.x * other.y - self.x * other.x
+        return self.x * other.y - self.y * other.x
 
-    def __lshift__(self, other: Float):
+    def __lshift__(self, other: float):
         """Returns rotated anticlockwise vector by an angle(radians) 'other'"""
 
         return Vec(
-            Float(cos(other) * self.x - sin(other) * self.y),
-            Float(sin(other) * self.x + cos(other) * self.y)
+            cos(other) * self.x - sin(other) * self.y,
+            sin(other) * self.x + cos(other) * self.y
         )
 
-    def __rshift__(self, other: Float):
+    def __rshift__(self, other: float):
         """Returns rotated clockwise vector by an angle(radians) 'other'"""
 
         return Vec(
-            Float(cos(other) * self.x + sin(other) * self.y),
-            Float(-sin(other) * self.x + cos(other) * self.y)
+            cos(other) * self.x + sin(other) * self.y,
+            -sin(other) * self.x + cos(other) * self.y
         )
+
+    def __xor__(self, other):
+        """Returns angle between self and other"""
+        return abs(atan2(self ** other, self * other))
 
     def __eq__(self, other):
         """Returns True if self equal other else False"""
-        return self.x == other.x and self.y == other.y
+        return equal_float(self.x, other.x) and equal_float(self.y, other.y)
 
 
 class Figure:
     """Class of all geometry 2D figures"""
 
-    square: Float
+    square: float
 
-    def count_square(self) -> Float:
+    def count_square(self) -> float:
         """Counts square of the figure"""
         pass
 
@@ -105,14 +111,14 @@ class Figure:
 class Line(Figure):
     """Lines and their properties"""
 
-    a: Float
-    b: Float
-    c: Float
+    a: float
+    b: float
+    c: float
     normal: Vec
     first_point: Vec
     second_point: Vec
 
-    def __init__(self, first_point: Vec = None, second_point: Vec = None, a: Float = -1, b: Float = 1, c: Float = 0):
+    def __init__(self, first_point: Vec = None, second_point: Vec = None, a: float = -1, b: float = 1, c: float = 0):
         if first_point is None or second_point is None:
             self.a = a
             """Coefficient A of line"""
@@ -120,9 +126,9 @@ class Line(Figure):
             """Coefficient B of line"""
             self.c = c
             """Coefficient C of line"""
-            self.first_point = Vec(Float(0), self[0])
+            self.first_point = Vec(0, self[0])
             """One of the point line include"""
-            self.second_point = Vec(Float(0), self[1])
+            self.second_point = Vec(0, self[1])
             """Another of the point line include"""
         else:
             self.first_point = first_point
@@ -142,7 +148,7 @@ class Line(Figure):
         """Counts normal of line"""
         return Vec(self.a, self.b)
 
-    def count_coefficient_a(self, point1: Vec = None, point2: Vec = None) -> Float:
+    def count_coefficient_a(self, point1: Vec = None, point2: Vec = None) -> float:
         """Counts coefficient A of line in Ax+By+C=0"""
 
         if point1 is None:
@@ -153,7 +159,7 @@ class Line(Figure):
 
         return point1.y - point2.y
 
-    def count_coefficient_b(self, point1: Vec = None, point2: Vec = None) -> Float:
+    def count_coefficient_b(self, point1: Vec = None, point2: Vec = None) -> float:
         """Counts coefficient B of line in Ax+By+C=0"""
 
         if point1 is None:
@@ -164,7 +170,7 @@ class Line(Figure):
 
         return point2.x - point1.x
 
-    def count_coefficient_c(self, point1: Vec = None, point2: Vec = None) -> Float:
+    def count_coefficient_c(self, point1: Vec = None, point2: Vec = None) -> float:
         """Counts coefficient C of line in Ax+By+C=0"""
 
         if point1 is None:
@@ -175,7 +181,7 @@ class Line(Figure):
 
         return (point1.x - point2.x) * point1.y + (point2.y - point1.y) * point1.x
 
-    def __getitem__(self, item, first=False):
+    def __getitem__(self, item, first=False) -> float:
         """Returns second(first if tag first is True) coordinate of point on line with X(Y if tag first is True) equal
             to item (counts value of y(x) or opposite). Example: a = Line((0, 0), (1, 2)); a[2] -> 4; a[6, True] -> 3"""
 
@@ -193,13 +199,11 @@ class Line(Figure):
             """Item is a point"""
             item: Vec
 
-            print("LINE:", item, Vec(item.x, self[item.x]))
-
             return Vec(item.x, self[item.x]) == item
 
         """Item is a segment"""
         item: Segment
-        return item.a == self.a and item.b == self.b and item.c == self.c
+        return equal_float(item.a, self.a) and equal_float(item.b, self.b) and equal_float(item.c, self.c)
 
     def __repr__(self):
         return "Line(" + \
@@ -230,18 +234,15 @@ class Segment(Line):
         self.square = self.count_square()
         """Length(square) of the segment"""
 
-    def count_square(self) -> Float:
+    def count_square(self) -> float:
         """Counts length(square) of the segment"""
         return (self.end_point - self.begin_point).len
 
     def __contains__(self, item: Vec) -> bool:
         """Returns True if item(point) belong to the segment(is a subset of the segment) else False"""
 
-        print("IAM:", self)
-        print(f"IN: {item} : {super(Segment, self).__contains__(item)} , {(item - self.begin_point) * (item - self.end_point)}")
-
         return super(Segment, self).__contains__(item) and \
-            (item - self.begin_point) * (item - self.end_point) < Float(0)
+            lt_float((item - self.begin_point) * (item - self.end_point), 0)
 
     # def intersection(self, other) -> Float:
     #     """Returns point of intersection of two segments"""
@@ -251,7 +252,7 @@ class Segment(Line):
 class Circle(Figure):
     """Circles and their properties"""
 
-    def __init__(self, centre_point: Vec, radius: Float):
+    def __init__(self, centre_point: Vec, radius: float):
         self.centre_point = centre_point
         """Position of centre od circle"""
         self.radius = radius
@@ -259,7 +260,7 @@ class Circle(Figure):
         self.square = self.count_square()
         """Square of circle"""
 
-    def count_square(self) -> Float:
+    def count_square(self) -> float:
         """Counts square of the circle"""
         return self.radius ** 2 * pi
 
@@ -268,23 +269,23 @@ class Circle(Figure):
 
         inter1, inter2 = None, None
 
-        desc = Float(-(4 * other.a ** 2 + 4 * other.b ** 2) * (
+        desc = -(4 * other.a ** 2 + 4 * other.b ** 2) * (
                 self.centre_point.x ** 2 + self.centre_point.y ** 2 - self.radius ** 2 + 2 * other.c *
                 self.centre_point.x / other.a + other.c ** 2 / other.a ** 2) / other.a ** 2 + (
                              -2 * other.a ** 2 * self.centre_point.y + 2 * other.a * other.b * self.centre_point.x + 2
-                             * other.b * other.c) ** 2 / other.a ** 4)
+                             * other.b * other.c) ** 2 / other.a ** 4
 
         count_y = lambda D: other.a ** 2 * (D - (
                 -2 * other.a ** 2 * self.centre_point.y + 2 * other.a * other.b * self.centre_point.x + 2 * other.b
                 * other.c) / other.a ** 2) / (2 * other.a ** 2 + 2 * other.b ** 2)
         count_x = lambda D: -other.b * count_y(D) / other.a - other.c / other.a
 
-        if desc == 0.0:
+        if equal_float(desc, 0):
             """There is only one solution(point)"""
             desc = 0
             inter1 = Vec(count_x(desc), count_y(desc))
             inter2 = None
-        elif desc > 0.0:
+        elif lt_float(0, desc):
             """There are two different solutions(points)"""
             desc = sqrt(desc)
             inter1 = Vec(count_x(desc), count_y(desc))
@@ -300,32 +301,21 @@ class Circle(Figure):
         inter1, inter2 = None, None
         """Points of intersection"""
 
-        print("First:", other, position)
-
         if other is None:
             if position is None:
                 return inter1, inter2
             other = Segment(position, self.centre_point)
 
-        print("Second:", other, position)
-
         intersection_line_circle = self.intersection_with_line(other)
         """Intersection of circle and line that contains Segment"""
 
-        print("LineInss:", intersection_line_circle)
-
-        if intersection_line_circle[0] is not None and intersection_line_circle[0] in other:
+        if intersection_line_circle[0] is not None and other.__contains__(intersection_line_circle[0]):
             """First point belong to Segment"""
-            print(111)
             inter1 = intersection_line_circle[0]
 
-        if intersection_line_circle[1] is not None and intersection_line_circle[1] in other:
+        if intersection_line_circle[1] is not None and other.__contains__(intersection_line_circle[1]):
             """Second point belong to Segment"""
-            print(222)
             inter2 = intersection_line_circle[1]
-
-        print("Points:", inter1, inter2)
-        print("Ans:", tuple(sorted([inter1, inter2], key=lambda elem: 1 if elem is None else -1)))
 
         return tuple(sorted([inter1, inter2], key=lambda elem: 1 if elem is None else -1))
 
@@ -335,7 +325,7 @@ class Circle(Figure):
         if type(item) is Vec:
             """Item is a point"""
             item: Vec
-            return (self.centre_point - item).len <= self.radius
+            return le_float((self.centre_point - item).len, self.radius)
 
         if type(item) is Line:
             """Item is a line"""
