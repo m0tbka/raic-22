2295ccc4f6e39d7585bed57e0b0e5ef4b4b792e1
m0tbka <66780351+m0tbka@users.noreply.github.com>
2022-07-15 00:33:31 +0500

    Some cosmetic changes

--- simulation.py
+++ simulation.py
@@ -3,38 +3,40 @@
 from strategy.float import equal_float, lt_float, le_float
 from strategy.geometry import Vec, Line, Segment, Circle
 from strategy.physics import Charge, ObstacleCharge, ZoneCharge
+from strategy.unit_custom import UnitAdv
 
 from math import pi
 
 
-def simulations_around(obsts: dict, zone: ZoneCharge, centre_pos: Vec, step: float, angle: float) -> Vec:
-    """Simulates all positions around 'centre_pos' in 'step' and on every 'angle' in radians.
-        Returns position with maximal charge"""
+def simulation_next_step(obsts: dict, zone: ZoneCharge, units: dict, my_unit: UnitAdv, angle: float) -> Vec:
+    """"""
 
-    step_vector = Vec(step, 0)
+    step_vector = Vec(my_unit.acceleration_limit_circle.radius, 0)
     angle_now = angle
-    simulated_pos = centre_pos + step_vector
-    maximum = [count_charge(obsts, zone, simulated_pos), simulated_pos]
-
-    cou = 0
+    simulated_pos = my_unit.acceleration_limit_circle.centre_point + step_vector
+    maximum = [count_charge(obsts, zone, units, simulated_pos), simulated_pos]
 
     while lt_float(angle_now, 2 * pi):
-        simulated_pos = centre_pos + (step_vector >> angle_now)
-        new_charge = count_charge(obsts, zone, simulated_pos)
+        simulated_pos = my_unit.acceleration_limit_circle.centre_point + (step_vector >> angle_now)
+
+        new_charge = count_charge(obsts, zone, units, simulated_pos)
+
         if lt_float(maximum[0], new_charge):
             maximum = [new_charge, simulated_pos]
-        angle_now += angle
-        cou += 1
 
-    print(f"Actions: {cou}; Angle: {angle}, {angle_now}")
+        angle_now += angle
+
+    print(maximum)
+
     return maximum[1]
 
 
-def count_charge(obsts: dict, zone: ZoneCharge, position: Vec) -> float:
+def count_charge(obsts: dict, zone: ZoneCharge, units: dict, position: Vec) -> float:
     """Returns summary charge at 'position'"""
 
     quantity = zone.get_charge_at_pos(position)
     for obs in obsts:
+
         quantity += obsts[obs].get_charge_at_pos(position)
 
     return quantity
